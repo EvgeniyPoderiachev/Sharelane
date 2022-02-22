be9fc31dfd22e903fed8ed657869d6a0418d1f80
Eugene <setminsk@gmail.com>
2022-02-22 21:12:35 +0300

    Initial commit

---
+++ src/test/java/SignUpTest.java
@@ -0,0 +1,83 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class SignUpTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void zipCode1() {
+        /*
+        1. Открыть браузер
+        2. Перейти по ссылке https://www.sharelane.com/cgi-bin/register.py
+        3. Вводим 5 цифр, например 12345
+        4. Нажать Continue
+        5. Проверить, что кнопка Register видна
+         */
+
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys("12345");
+        driver.findElement(By.cssSelector("[value=Continue]")).click();
+        boolean isDisplayed = driver.findElement(By.cssSelector("[value=Register]")).isDisplayed();
+        assertTrue(isDisplayed);
+
+
+    }
+
+    @Test
+    public void zipCode2() {
+
+        /*
+        1. Открыть браузер
+        2. Перейти по ссылке https://www.sharelane.com/cgi-bin/register.py
+        3. Вводим 4 цифры, например 1234
+        4. Нажать Continue
+        5. Проверить, что отображается сообщение с ошибкой
+        */
+
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys("1234");
+        driver.findElement(By.cssSelector("[value=Continue]")).click();
+        boolean isDisplayed = driver.findElement(By.className("error_message")).isDisplayed();
+        assertTrue(isDisplayed);
+
+
+    }
+
+    @Test
+    public void zipCode3() {
+
+        /*
+        1. Открыть браузер
+        2. Перейти по ссылке https://www.sharelane.com/cgi-bin/register.py
+        3. Вводим 6 цифр, например 123456
+        4. Нажать Continue
+        5. Проверить, что отображается сообщение с ошибкой
+        */
+
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys("123456");
+        driver.findElement(By.cssSelector("[value=Continue]")).click();
+        boolean isDisplayed = driver.findElement(By.className("error_message")).isDisplayed();
+        assertTrue(isDisplayed);
+
+
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+}
